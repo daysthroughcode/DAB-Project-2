11bb7d946af7123e4f2cacffd76b6e94470fe2e2
Advali Tech Pty Ltd <bimalkprabha24@gmail.com>
2021-01-16 13:57:47 +0800

    updated flask

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect,jsonify,render_template
 app = Flask(__name__)
-import psycopg2
-# from psycopg2 import Error
+
 from sqlalchemy import create_engine
 import pandas as pd
 
@@ -9,35 +8,17 @@ import pandas as pd
 engine = create_engine(f"postgresql://postgres:asd@localhost:5432/DAB-Project-2")
 connection = engine.connect()
 
-# connection = psycopg2.connect(user="postgres",
-#                                   password="asd",
-#                                   host="127.0.0.1",
-#                                   port="5432",
-#                                   database="DAB-Project-2")
-
-#     # Create a cursor to perform database operations
-# cursor = connection.cursor()
-    # Print PostgreSQL details
-
-    # Executing a SQL query
-# cursor.execute('SELECT * FROM airline_crash')
-#     # Fetch result
-# record = cursor.fetchall()
-
-
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route("/data")
 def data():
     
     df = pd.read_sql("SELECT * FROM airline_crash ",connection)
     records = df.to_json()
-    # Fetch result
-    # for rows in records:
     return records
-    # return jsonify(records)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
