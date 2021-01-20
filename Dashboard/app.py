f6a62a5e1954b333f171ae912630fe238d23fc90
daysthroughcode <58594754+daysthroughcode@users.noreply.github.com>
2021-01-20 17:42:32 +0800

    modifications

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,12 +1,12 @@
 import pandas as pd
+from config import pg_pwd
 from sqlalchemy import create_engine
 from flask import Flask, render_template, redirect, jsonify, render_template
 app = Flask(__name__)
 
-
 # for pandas reqd_sql
 engine = create_engine(
-    f"postgresql://postgres:4gttnp5t@localhost:5432/airline_crash")
+    f"postgresql://postgres:{pg_pwd}@localhost:5432/airline_crash")
 connection = engine.connect()
 
 
@@ -19,8 +19,35 @@ def hello_world():
 def data():
 
     df = pd.read_sql("SELECT * FROM airline_data ", connection)
-    records = df.to_json()
+    df = df.groupby(['ac_type', 'operator'])['fatalities'].sum().reset_index()
+    records = df.to_json(orient='records')
     return records
+
+
+@app.route("/data0")
+def data0():
+
+    df = pd.read_sql("SELECT * FROM airline_data ", connection)
+    df = df.groupby(['decade', 'operator'])['fatalities'].sum().reset_index()
+    #df = df.nlargest(15, 'fatalities')
+    records = df.to_json(orient='records')
+    return records
+
+
+@app.route("/vis1")
+def pointcloud():
+    return render_template('pointcloud.html')
+
+
+@app.route("/vis2")
+def donut():
+    return render_template('donut.html')
+
+
+@app.route("/vis3")
+@app.route("/default")
+def default():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
